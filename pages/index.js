fb9abb89da88751ebf3b3efebe3a40cfc914150f
ryzhak <ryzhak.vladimir@gmail.com>
2022-12-07 02:15:55 +0300

    fix: env 6

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ allPostsData }) {
               href={`https://github.com/ubiquity/ubiquity-dollar/commit/${process.env.COMMIT_REF}`}
               target="_blank"
             >
-              {String(process.env.COMMIT_REF).substring(0,8)}
+              1.0.0
             </a>
           </div>
       </section>
